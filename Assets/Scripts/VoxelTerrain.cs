6abeda2a46a545ebfb2bfa77e9999320cb31413d
Remy Maetz <rmaetz@gmail.com>
2024-02-07 19:21:34 +0100

    Add side faces and basic coloring

--- Assets/Scripts/VoxelTerrain.cs
+++ Assets/Scripts/VoxelTerrain.cs
@@ -7,6 +7,7 @@ using UnityEngine.Rendering;
 public class VoxelTerrain : MonoBehaviour
 {
     public Texture2D heightMap;
+    public Texture2D colorMap;
     public Material referenceMaterial;
 
     public Vector2 size = new Vector2(256, 256);
@@ -54,73 +55,127 @@ public class VoxelTerrain : MonoBehaviour
     {
         Chunk chunk = new Chunk();
 
+        bool doXFace = cx > 0;
+        bool doZFace = cy > 0;
+
         chunk.material = Instantiate( referenceMaterial );
 
         var mesh = new Mesh();
         var vertices = new List<Vector3>();
         var normals = new List<Vector3>();
+        var uvs = new List<Vector2>();
         var triangles = new List<int>();
+        var vertexIndex = 0;
+
+        void AddQuad( Vector3 origin, Vector3 right, Vector3 up, Vector3 normal, Vector2 uv_BottomLeft, Vector2 uv_TopRight )
+        {
+            vertices.Add(origin);
+            vertices.Add(origin + right);
+            vertices.Add(origin + right + up);
+            vertices.Add(origin + up);
+
+            normals.Add(normal);
+            normals.Add(normal);
+            normals.Add(normal);
+            normals.Add(normal);
+
+            uvs.Add(uv_BottomLeft);
+            uvs.Add(new Vector2(uv_TopRight.x, uv_BottomLeft.y));
+            uvs.Add(uv_TopRight);
+            uvs.Add(new Vector2(uv_BottomLeft.x, uv_TopRight.y));
+
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex + 1);
+            triangles.Add(vertexIndex + 2);
+            triangles.Add(vertexIndex);
+            triangles.Add(vertexIndex + 3);
+
+            vertexIndex += 4;
+        }
 
         var pixelOffsetX = cx * chunkSize;
         var pixelOffsetY = cy * chunkSize;
-        var blockWidth = Mathf.Min(chunkSize, heightMap.width - pixelOffsetX );
-        var blockHeight = Mathf.Min(chunkSize, heightMap.height - pixelOffsetY );
+        var blockWidth = chunkSize;
+        var blockHeight = chunkSize;
+        var startX = 0;
+        var startY = 0;
+        if (doXFace)
+        {
+            pixelOffsetX --;
+            blockWidth++;
+            startX++;
+        }
+        if (doZFace)
+        {
+            pixelOffsetY--;
+            blockHeight++;
+            startY++;
+        }
+        blockWidth = Mathf.Min(blockWidth, heightMap.width - pixelOffsetX);
+        blockHeight = Mathf.Min(blockHeight, heightMap.height - pixelOffsetY);
 
-        var deltaX = blockWidth / scale.x;
-        var deltaY = blockHeight / scale.z;
+        var deltaX = chunkSize / scale.x;
+        var deltaZ = chunkSize / scale.z;
 
         var dx = new Vector3( deltaX, 0, 0);
-        var dy = new Vector3( 0, 0, deltaY);
-        var dxy = new Vector3(deltaX, 0, deltaY);
+        var dz = new Vector3( 0, 0, deltaZ);
+        var dh = Vector3.zero;
+        var du = 1.0f / colorMap.width;
+        var dv = 1.0f / colorMap.height;
 
         var position = Vector3.zero;
-        var dh = Vector3.zero;
+        var uv_BottomLeft = Vector2.zero;
+        var uv_TopRight = Vector2.one;
 
         var heights = heightMap.GetPixels(pixelOffsetX, pixelOffsetY, blockWidth, blockHeight, 0);
-        var i = 0;
-        var pi = 0;
+        var pixelIndex = startY * blockWidth + startX;
 
-        for (var y = 0; y < blockHeight; y++)
+        for (var z = startY; z < blockHeight; z++)
         {
-            for (var x = 0; x < blockWidth; x++)
+            for (var x = startX; x < blockWidth; x++)
             {
                 // top face
-                position.y = heights[pi].r * scale.y;
-                pi++;
+                position.y = heights[pixelIndex].r * scale.y;
 
-                vertices.Add(position);
-                vertices.Add(position+dx);
-                vertices.Add(position+dxy);
-                vertices.Add(position+dy);
+                uv_BottomLeft.x = Mathf.Floor(heights[pixelIndex].r * colorMap.width)*du;
+                uv_TopRight.x = uv_BottomLeft.x + du;
+                uv_BottomLeft.y = 1.0f - dv;
+                uv_TopRight.y = 1.0f;
 
-                normals.Add(Vector3.up);
-                normals.Add(Vector3.up);
-                normals.Add(Vector3.up);
-                normals.Add(Vector3.up);
+                AddQuad(position, dx, dz, Vector3.up, uv_BottomLeft, uv_TopRight);
 
-                triangles.Add(i);
-                triangles.Add(i + 2);
-                triangles.Add(i+1);
-                triangles.Add(i+2);
-                triangles.Add(i);
-                triangles.Add(i+3);
-
-                i += 4;
-
-                // -z vertical face
-                if (y > 0)
+                // -x vertical face
+                if (x > 0 || doXFace)
                 {
-
+                    dh.y = heights[pixelIndex-1].r * scale.y - position.y;
+                    if (Mathf.Abs(dh.y) > Mathf.Epsilon)
+                        AddQuad(position, dz, dh, Vector3.left, uv_BottomLeft, uv_TopRight);
                 }
 
+                // -z vertical face
+                if (z > 0 || doZFace)
+                {
+                    dh.y = heights[pixelIndex - blockWidth].r * scale.y - position.y;
+                    if (Mathf.Abs(dh.y) > Mathf.Epsilon)
+                        AddQuad(position+dx, -dx, dh, Vector3.back, uv_BottomLeft, uv_TopRight);
+                }
+
+                pixelIndex++;
                 position.x += deltaX;
             }
+            // Offset the pixel index to skip first pixel of a line if we are also doing the first X face of the chunk
+            if (doXFace)
+            {
+                pixelIndex++;
+            }
             position.x = 0;
-            position.z += deltaY;
+            position.z += deltaZ;
         }
 
         mesh.vertices = vertices.ToArray();
         mesh.normals = normals.ToArray();
+        mesh.uv = uvs.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateBounds();
 
@@ -142,11 +197,13 @@ public class VoxelTerrain : MonoBehaviour
     void RenderTerrain()
     {
         var renderParams = new RenderParams();
+        renderParams.layer = gameObject.layer;
+        renderParams.renderingLayerMask = GraphicsSettings.defaultRenderingLayerMask;
+        renderParams.material = referenceMaterial;
+        renderParams.shadowCastingMode = ShadowCastingMode.On;
+        renderParams.receiveShadows = true;
         foreach (var chunk in chunks)
         {
-            renderParams.layer = gameObject.layer;
-            renderParams.renderingLayerMask = GraphicsSettings.defaultRenderingLayerMask;
-            renderParams.material = chunk.material;
             Graphics.RenderMesh( renderParams, chunk.mesh, 0, chunk.matrix);
         }
     }
@@ -169,13 +226,14 @@ public class VoxelTerrain : MonoBehaviour
         if (!drawGizmos) return;
 
         var chunksScale = new Vector3(size.x / chunkSize, height, size.y / chunkSize);
-
+        Random.InitState( 42 );
         foreach (var chunk in chunks)
         {
+            Gizmos.color = Color.HSVToRGB(Random.value, 1, 1);
             Gizmos.matrix = chunk.matrix;
             //Gizmos.DrawWireCube(chunksScale * 0.5f, chunksScale);
-            Gizmos.DrawWireCube( chunk.mesh.bounds.center , chunk.mesh.bounds.size);
-            //Gizmos.DrawWireMesh(chunk.mesh);
+            //Gizmos.DrawWireCube( chunk.mesh.bounds.center , chunk.mesh.bounds.size);
+            Gizmos.DrawWireMesh(chunk.mesh);
         }
     }
 }
